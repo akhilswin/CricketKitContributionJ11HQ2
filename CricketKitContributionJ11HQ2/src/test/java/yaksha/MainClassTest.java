02cdac49e924d20731a33baf13acf9f39358170a
Akhilesh <Akhilesh@Akhilesh-PC>
2020-09-11 19:39:21 +0530

    CricketKitContributionJ11HQ2

--- CricketKitContributionJ11HQ2/src/test/java/yaksha/MainClassTest.java
+++ CricketKitContributionJ11HQ2/src/test/java/yaksha/MainClassTest.java
@@ -9,7 +9,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class MainClassTest {
-	
+
 	@Test
 	public void testExceptionConditon() throws Exception {
 
@@ -26,15 +26,20 @@ class MainClassTest {
 	void testObtainPurchaseWithAmount() throws Exception {
 		// Test will pass
 		List<Purchase> purchases = new ArrayList<Purchase>();
+		List<String> expectedList = new ArrayList<String>();
+		List<String> resultList = new ArrayList<String>();
 		String str = "1,12-12-2012,John,Bat,1500,5,Ball,60,2,Stump,340,15,helmet,925,8,Pad,600,20,Gloves,450,35,Guard,120,4";
-		String str1 = "3,12-12-2012,Jack,Bat,1500,7,Ball,60,1,Stump,340,5,helmet,925,5,Pad,600,15";
 		Purchase p = Purchase.obtainPurchaseWithAmount(str);
 		purchases.add(p);
+		String str1 = "3,12-12-2012,Jack,Bat,1500,7,Ball,60,1,Stump,340,5,helmet,925,5,Pad,600,15";
 		p = Purchase.obtainPurchaseWithAmount(str1);
 		purchases.add(p);
 		Collections.sort(purchases);
-		String result = purchases.toString();
-		String expectedEesult = "3          Jack            25885.0\r" + "1          John            48350.0";
-		yakshaAssert(currentTest(), (expectedEesult.equals(result) ? "true" : "false"), businessTestFile);
+		expectedList.add("3          Jack            25885.0");
+		expectedList.add("1          John            48350.0");
+		for (int i = 0; i < purchases.size(); i++) {
+			resultList.add(purchases.get(i).toString());
+		}
+		yakshaAssert(currentTest(), (expectedList.containsAll(resultList) ? "true" : "false"), businessTestFile);
 	}
 }
